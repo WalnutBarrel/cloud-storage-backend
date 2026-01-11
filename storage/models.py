6376551efe692bd45730872e84c019d0d159a1be
WalnutBarrel <87712034+WalnutBarrel@users.noreply.github.com>
2026-01-11 22:24:46 +0530

    Add file size tracking

--- storage/models.py
+++ storage/models.py
@@ -7,6 +7,14 @@ class Folder(models.Model):
     def __str__(self):
         return self.name
 
+class StorageAccount(models.Model):
+    name = models.CharField(max_length=50)
+    cloud_name = models.CharField(max_length=100)
+    api_key = models.CharField(max_length=100)
+    api_secret = models.CharField(max_length=100)
+    used_bytes = models.BigIntegerField(default=0)
+    limit_bytes = models.BigIntegerField(default=24 * 1024 * 1024 * 1024)
+
 
 class File(models.Model):
     name = models.CharField(max_length=255)
@@ -21,6 +29,9 @@ class File(models.Model):
     )
     size = models.BigIntegerField(default=0)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    storage_account = models.ForeignKey(StorageAccount, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
