fbb6bf1ba3d74308f55742af42a5c92e9122657c
WalnutBarrel <87712034+WalnutBarrel@users.noreply.github.com>
2026-01-11 19:48:11 +0530

    Add folder model and file-folder relation

--- storage/models.py
+++ storage/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
+class Folder(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
 class File(models.Model):
     name = models.CharField(max_length=255)
     file_url = models.URLField()
-    resource_type = models.CharField(max_length=20)  # image, video, raw
+    resource_type = models.CharField(max_length=20)
+    folder = models.ForeignKey(
+        Folder,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="files"
+    )
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
